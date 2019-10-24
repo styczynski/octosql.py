29da4e395c3923334bc8457698b88eb0bd0fba36
Piotr Styczyński <piotrs@radcode.co>
2019-10-24 10:22:05 +0200

    Try to fix Travis

--- setup_native.sh
+++ setup_native.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.13 bash)" && go install ./ ... && go build -o "$1" -buildmode=c-archive "$2"
+eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.13 bash)" && go install ./... && go build -o "$1" -buildmode=c-archive "$2"
