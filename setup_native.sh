423e98c520947b8bf376ea9dcbad8d540ebd11a3
Piotr Styczyński <piotrs@radcode.co>
2019-10-25 13:52:58 +0200

    Better archive building

--- setup_native.sh
+++ setup_native.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd src/custom_storage && make all && cd ../..
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=1.13 bash)" && go install ./... && go build -o "$1" -buildmode=c-archive "$2"
