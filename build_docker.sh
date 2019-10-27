2339a90b0a997289279596a175047f7dcdd67e8c
Piotr Styczyński <piotrs@radcode.co>
2019-10-27 17:37:56 +0100

    Fix Docker deployment

--- build_docker.sh
+++ build_docker.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Will create docker image with version label: ${OCTOSQL_CURRENT_VERSION}"
-if [ -z "$OCTOSQL_CURRENT_VERSION" ]
+VERCMD="$(cat setup.cfg | grep 'current_version = ')"
+VERCMD=${VERCMD//[[:blank:]]/}
+eval "export $VERCMD"
+
+echo "Will create docker image with version label: ${current_version}"
+if [ -z "$current_version" ]
 then
       echo "Failed to get the project version. It may not be installed. Please run make to build the project first."
       exit 1
 else
       echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
       docker build -t octosql-py .
-      docker tag octosql-py styczynski/octosql-py:${OCTOSQL_CURRENT_VERSION}
-      docker push styczynski/octosql-py:${OCTOSQL_CURRENT_VERSION}
+      docker tag octosql-py styczynski/octosql-py:${current_version}
+      docker push styczynski/octosql-py:${current_version}
 fi
